c3052434452a795eb2d5219288145e3783cb86ac
nncatan <nncatan@gmail.com>
2020-12-08 15:29:45 +0800

    Reorganized the folder so that only TPD_Tools folder needs to be dragged into .nuke

---
+++ TPD_Tools/TPD_Menu.py
@@ -0,0 +1,3 @@
+import nuke
+
+nuke.menu('Nodes').addMenu('TPD_Tools', icon="TPD_Icon.png")
--- TPD_Tools/__init__.py
+++ TPD_Tools/__init__.py
@@ -7,9 +7,14 @@
 #  Last Updated: 12.04.2020
 # --------------------------------------------------------
 
+import nuke
+
 # ----- DEFINE CUSTOM FOLDER STRUCTURE -------------------
 
 nuke.pluginAddPath('./TPD_Tools/gizmos')
 nuke.pluginAddPath('./TPD_Tools/icons')
 nuke.pluginAddPath('./TPD_Tools/python')
 nuke.pluginAddPath('./TPD_Tools/templates')
+
+import TPD_Menu
+import TPD_Roto
